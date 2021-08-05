30d88a351ed394f6660f87d8c4c1d8dcbc50bb3a
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-08-05 11:03:13 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -17,7 +17,7 @@ pipeline{
            sh 'aws --version'
            sh 'aws configure set region us-east-1'
            sh " aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 993745358053.dkr.ecr.us-east-1.amazonaws.com "
-       sh "aws ecr create-repository --repository-name ${STACKNAME}"
+       //sh "aws ecr create-repository --repository-name ${STACKNAME}"
          sh "docker tag ${STACKNAME}:latest ${ECR_REGISTRY}/${STACKNAME}"
         sh "docker push ${ECR_REGISTRY}/${STACKNAME}"
          sh "docker rmi ${ECR_REGISTRY} ${STACKNAME}"
