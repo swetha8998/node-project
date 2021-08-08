6c060d5aeb2fd15c5995e01be46bcda8a63ba73d
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-08-08 17:22:31 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,4 +1,4 @@
-final STACKNAME= 'node-projct'
+final STACKNAME= 'node'
 final ACCOUNT= '993745358053'
 def version = currentBuild.number
 final ECR_REGISTRY='993745358053.dkr.ecr.us-east-1.amazonaws.com'
@@ -13,13 +13,15 @@ pipeline{
     stage('Building'){
    steps{
        script{
-           sh 'docker build . -f Dockerfile -t node-projct'
+         sh 'aws configure set region us-east-1'
+           sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 993745358053.dkr.ecr.us-east-1.amazonaws.com'
+           sh 'docker build -t node .'
            sh 'aws --version'
-           sh 'aws configure set region us-east-1'
-           sh " aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 993745358053.dkr.ecr.us-east-1.amazonaws.com "
+                   
        sh "aws ecr create-repository --repository-name ${STACKNAME}"
-         sh "docker tag ${STACKNAME}:latest ${ECR_REGISTRY}/${STACKNAME}"
-        sh "docker push ${ECR_REGISTRY}/${STACKNAME}"
+         
+         sh 'docker tag node:latest 993745358053.dkr.ecr.us-east-1.amazonaws.com/node:latest'
+        sh "docker push 993745358053.dkr.ecr.us-east-1.amazonaws.com/node:latest"
          //sh "docker rmi ${ECR_REGISTRY} ${STACKNAME}"
         sh 'echo "image is pushed"'
          
